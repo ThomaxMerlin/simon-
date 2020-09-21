b448312aa99353075d94bd37cf714348ce00a3cc
daliborstakic <stakicdalibor64@gmail.com>
2020-09-21 14:59:30 +0200

    added color classes

--- colors.py
+++ colors.py
@@ -1,3 +1,4 @@
+import pygame
 from abc import ABC, abstractmethod
 
 class Color(ABC):
@@ -20,6 +21,54 @@ class Color(ABC):
         pass
 
     @abstractmethod
-    def play(self, surface):
+    def play(self):
         """ Plays coresponding sound """
+        pass
+
+class Red(Color):
+    def __init__(self, x, y, box_size):
+        super().__init__()
+        self.color = (255, 0, 0)
+
+    def draw(self, surface):
+        """ Draws the box """
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.box_size, self._box_size))
+
+    def play(self):
+        pass
+
+class Green(Color):
+    def __init__(self, x, y, box_size):
+        super().__init__()
+        self.color = (0, 255, 0)
+
+    def draw(self, surface):
+        """ Draws the box """
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.box_size, self._box_size))
+
+    def play(self):
+        pass
+
+class Blue(Color):
+    def __init__(self, x, y, box_size):
+        super().__init__()
+        self.color = (0, 0, 255)
+
+    def draw(self, surface):
+        """ Draws the box """
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.box_size, self._box_size))
+
+    def play(self):
+        pass
+
+class Yellow(Color):
+    def __init__(self, x, y, box_size):
+        super().__init__()
+        self.color = (255, 255, 0)
+
+    def draw(self, surface):
+        """ Draws the box """
+        pygame.draw.rect(surface, self.color, (self.x, self.y, self.box_size, self._box_size))
+
+    def play(self):
         pass
