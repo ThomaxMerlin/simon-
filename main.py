ae64c288d28d19abb683fa6e866f0f1dd0427ccb
daliborstakic <stakicdalibor64@gmail.com>
2020-09-23 10:46:19 +0200

    added loss screen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
 import random
-
-from pygame.mixer import fadeout
 from colors import *
 
 pygame.init()
@@ -11,6 +9,10 @@ WIDTH = 600
 
 # Game constants
 BOX_SIZE = 200
+
+# Loss font
+loss_font = pygame.font.SysFont('Arial', 60)
+loss_text = loss_font.render("You lost!", 1, (255, 255, 255))
 
 def draw(surface, colors_):
     """ Draws the screen """
@@ -25,6 +27,7 @@ def is_clicked(pos, color):
     """ If the box is clicked """
     m_x, m_y = pos
 
+    # Checking for the mouse coordinates
     if color.x < m_x < color.x + color.box_size and color.y < m_y < color.y + color.box_size:
         return True
     
@@ -37,6 +40,13 @@ def play_clicked(surface, color):
     pygame.display.update()
     pygame.time.delay(250)
     pygame.draw.rect(surface, color.color, (color.x, color.y, BOX_SIZE, BOX_SIZE))
+
+def display_message(surface, text):
+    """ Displays a centered text """
+    surface.fill((0, 0, 0))
+    surface.blit(text, ((WIDTH - text.get_width()) // 2, (WIDTH - text.get_height()) // 2))
+    pygame.display.update()
+    pygame.time.delay(500)
 
 def main(surface):
     """ Main function """
@@ -55,6 +65,7 @@ def main(surface):
     player_pattern = []
 
     while run:
+        # Drawing the screen
         draw(surface, colors_)
 
         for event in pygame.event.get():
@@ -68,15 +79,20 @@ def main(surface):
                         player_pattern.append(color)
                         play_clicked(surface, color)
 
-                        if pattern[pattern.index(color)] != player_pattern[player_pattern.index(color)]:
-                            run = False
+                        # Checking if the user entered the wrong pattern
+                        for i in range(len(pattern)):
+                            if pattern[i] != player_pattern[i]:
+                                display_message(surface, loss_text)
+                                pygame.quit()
 
+        # If the user has completed the pattern
         if len(pattern) == len(player_pattern):
             pygame.time.delay(500)
             pattern.append(random.choice(colors_))
             player_pattern = []
             computers_turn = True
 
+        # Showing the pattern
         if computers_turn:
             pygame.time.delay(500)
 
